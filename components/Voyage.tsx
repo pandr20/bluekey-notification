20f34aa9c94556d07071e3b3ac93baf838fe31ff
pandr20 <71690281+pandr20@users.noreply.github.com>
2023-04-04 12:27:55 +0200

    Update Voyage.tsx

--- components/Voyage.tsx
+++ components/Voyage.tsx
@@ -2,23 +2,22 @@ import React from "react";
 import VoyageData from "../data/VoyageData.json";
 
 type Voyageprops = {
-  voyage: {
-    id: number;
-    counterpart: string;
-    cp_date: string;
-    laycan_range: string;
-    eta_load: string;
-    loading_port: string;
-    eta_disch: string;
-    discharge_port: string;
-    freight: number;
-    status: string;
+    voyage: {
+      id: number;
+      counterpart: string;
+      cp_date: string;
+      laycan_range: string;
+      eta_load: string;
+      loading_port: string;
+      eta_disch: string;
+      discharge_port: string;
+      freight: number;
+      status: string;
+    };
   };
-};
-
 const Voyage: React.FC<Voyageprops> = ({ voyage }) => {
   return (
-    <div className="border border-gray-300 rounded-lg shadow-lg p-6">
+    <div className="border border-gray-300 rounded-lg shadow-lg p-6 m-6">
       <h2 className=" text-white text-lg font-bold mb-4">Voyage {voyage.id}</h2>
       <div className="flex flex-col sm:flex-row">
         <div className="flex-1 mb-2 sm:mb-0">
@@ -38,34 +37,21 @@ const Voyage: React.FC<Voyageprops> = ({ voyage }) => {
         <div className="flex-1 mb-2 sm:mb-0">
           <p className="font-bold text-white">Cp Date:</p>
           <p className="text-dimWhite">{voyage.cp_date}</p>
+          <p className="font-bold text-white mt-2">Laycan Range:</p>
+          <p className="text-dimWhite">{voyage.laycan_range}</p>
         </div>
         <div className="flex-1 mb-2 sm:mb-0">
           <p className="font-bold text-white">Loading port:</p>
           <p className="text-dimWhite">{voyage.loading_port}</p>
+          <p className="font-bold text-white mt-2">Discharge port:</p>
+          <p className="text-dimWhite">{voyage.discharge_port}</p>
         </div>
         <div className="flex-1 mb-2 sm:mb-0">
           <p className="font-bold text-white">ETA Discharge:</p>
           <p className="text-dimWhite">{voyage.eta_disch}</p>
-        </div>
-      </div>
-      <div className="flex flex-col sm:flex-row">
-        <div className="flex-1 mb-2 sm:mb-0">
-          <p className="font-bold text-white">Laycan Range:</p>
-          <p className="text-dimWhite">{voyage.laycan_range}</p>
-        </div>
-        <div className="flex-1 mb-2 sm:mb-0">
-          <p className="font-bold text-white">Discharge port:</p>
-          <p className="text-dimWhite">{voyage.discharge_port}</p>
-        </div>
-        <div className="flex-1"></div>
-      </div>
-      <div className="flex flex-col sm:flex-row">
-        <div className="flex-1 mb-2 sm:mb-0">
           <p className="font-bold text-white">Status:</p>
           <p className="text-dimWhite">{voyage.status}</p>
         </div>
-        <div className="flex-1"></div>
-        <div className="flex-1"></div>
       </div>
     </div>
   );
