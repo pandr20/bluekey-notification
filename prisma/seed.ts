de80129e6e6a592336a708aa86cd7ee4ed4400f4
Rasmus <68448027+ras-e@users.noreply.github.com>
2023-04-15 16:30:13 +0200

    Updated seed file
    - Now matches the voyage data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -24,24 +24,54 @@ async function main() {
 
     const service1 = await prisma.service.create({
       data: {
-        data: 'Service Two Data',
-        users: {
+        user: {
           connect: {
-            id: user2.id,
+            id: user1.id,
           },
         },
+        counterpart: 'ABC Shipping Co.',
+        cp_date: new Date('2022-01-05'),
+        laycan_range: '2022-01-10 - 2022-01-15',
+        eta_load: new Date('2022-01-12'),
+        loading_port: 'Port A',
+        eta_disch: new Date('2022-01-25'),
+        discharge_port: 'Port B',
+        freight: 7500,
+        status: 'ongoing',
       },
     })
 
     const service2 = await prisma.service.create({
       data: {
-        data: 'Service Two Data',
-        users: {
+        user: {
           connect: {
             id: user2.id,
           },
         },
+        counterpart: 'DEF Shipping Co.',
+        cp_date: new Date('2022-02-10'),
+        laycan_range: '2022-02-15 - 2022-02-25',
+        eta_load: new Date('2022-02-18'),
+        loading_port: 'Port C',
+        eta_disch: new Date('2022-03-05'),
+        discharge_port: 'Port D',
+        freight: 10000,
+        status: 'completed',
       },
+    })
+
+    const service3 = await prisma.service.create({
+      data: {
+        counterpart: 'GHI Shipping Co.',
+        cp_date: new Date('2022-03-20'),
+        laycan_range: '2022-03-25 - 2022-04-05',
+        eta_load: new Date('2022-04-01'),
+        loading_port: 'Port E',
+        eta_disch: new Date('2022-04-15'),
+        discharge_port: 'Port F',
+        freight: 12500,
+        status: 'ongoing',
+      }
     })
 
     const notification1 = await prisma.notification.create({
@@ -120,7 +150,7 @@ async function main() {
       },
     })
 
-  console.log({ user1, user2, service1, service2, notification1, notification2, notification3, notification4 })
+  console.log({ user1, user2, service1, service2, service3, notification1, notification2, notification3, notification4 })
 }
 
 main()
