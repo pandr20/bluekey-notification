069eef51d97b2f3a440b72edefc6224a286cb045
pandr20 <71690281+pandr20@users.noreply.github.com>
2023-05-09 20:35:38 +0200

    Added styling for string and change notification fetch and style

--- components/NotificationBell.tsx
+++ components/NotificationBell.tsx
@@ -4,28 +4,39 @@ import StateDropDown from "./StateDropDown";
 function NotificationBell() {
   const [notifications, setNotifications] = useState([]);
   const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const res = await fetch("/api/getNotification");
-        if (!res.ok) {
-          throw new Error("Failed to fetch Notification");
-        }
-        const result = await res.json();
-        setNotifications(result);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-        setLoading(false);
+
+  const fetchNotifications = async () => {
+    try {
+      const res = await fetch("/api/getNotification");
+      if (!res.ok) {
+        throw new Error("Failed to fetch Notification");
       }
+      const result = await res.json();
+      setNotifications(result);
+    } catch (error) {
+      console.error(error);
     }
-    fetchData();
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const pollNotifications = async () => {
+      while (isMounted) {
+        await fetchNotifications();
+        await new Promise((resolve) => setTimeout(resolve, 5000)); // wait 5 seconds before polling again
+      }
+    };
+    pollNotifications();
+
+    return () => {
+      isMounted = false;
+    };
   }, []);
 
   const [showNotifications, setShowNotifications] = useState(false);
 
   const handleClick = () => {
-    // Toggle notifications visibility
     setShowNotifications(!showNotifications);
   };
 
@@ -44,32 +55,44 @@ function NotificationBell() {
         )}
       </button>
       {showNotifications && notifications.length > 0 && (
-        <div className="absolute top-8 right-0 w-48 bg-gray-700 rounded-md shadow-lg py-2 px-3 mt-2 max-h-64 overflow-y-scroll scroll-smooth">
-          {notifications.map(
-            (notification: {
-              id: string;
-              userId: string;
-              service: [];
-              serviceId: string;
-              state: string;
-              created_at: string;
-              isReady: boolean;
-              priority: number;
-              message: string;
-            }) => (
-              <div key={notification.id} className="py-2 break-words">
-                <span className="text-white">
-                  <p className="p-0">Message: {notification.message}</p>
-                  <p className="p-0">Created at: {notification.created_at}</p>
-                  <p className="p-0">ServiceId: {notification.serviceId}</p>
-                  <p className="p-0">State: {notification.state}</p>
-
-                  <StateDropDown state={""} />
-                </span>
-                <hr className="border-gray-300 mt-3" />
-              </div>
-            )
-          )}
+        <div className="absolute top-8 right-0 w-60 bg-gray-700 rounded-md shadow-lg py-2 px-3 mt-2 max-h-64 overflow-y-scroll scroll-smooth">
+          {notifications
+            .slice()
+            .reverse()
+            .map(
+              (notification: {
+                id: string;
+                userId: string;
+                service: [];
+                serviceId: string;
+                state: string;
+                created_at: string;
+                isReady: boolean;
+                priority: number;
+                message: string;
+              }) => (
+                <div key={notification.id} className="py-2 break-words">
+                  <span className="text-white">
+                    <p
+                      className="p-0 break-words w-full pl-4 "
+                      dangerouslySetInnerHTML={{
+                        __html: `Message: ${notification.message.replace(
+                          /\n/g,
+                          "<br>"
+                        )}`,
+                      }}
+                    ></p>
+                    <p className="p-1 pt-2">
+                      Created at: {notification.created_at}
+                    </p>
+                    <p className="p-1">ServiceId: {notification.serviceId}</p>
+                    <p className="p-1">State: {notification.state}</p>
+                    <StateDropDown state={""} />
+                  </span>
+                  <hr className="border-gray-300 mt-3" />
+                </div>
+              )
+            )}
         </div>
       )}
     </div>
