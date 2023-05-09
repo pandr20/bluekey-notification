e9797c5832cf0f58767e4fd828c0790b9b6937ec
pandr20 <71690281+pandr20@users.noreply.github.com>
2023-05-09 21:25:00 +0200

    Update mqttPublisher.js

--- mqtt/mqttPublisher.js
+++ mqtt/mqttPublisher.js
@@ -1,43 +1,45 @@
-import mqtt from 'mqtt';
-import { createNotification } from '../prisma/queries/prismaQueries.js';
+import mqtt from "mqtt";
+import { createNotification } from "../prisma/queries/prismaQueries.js";
 
-const mqttBrokerUrl = 'mqtt://mqtt:1883';
+const mqttBrokerUrl = "mqtt://mqtt:1883";
 
-//Should be added in the respective Next.js file 
+//Should be added in the respective Next.js file
 //Or in Publisher & Subscriber IF it doesn't matter if publ & sub has the same clientID- If so, remove clientId in the function
-  //import { v4 as uuidv4 } from 'uuid';
-  //const clientId = uuidv4(); // Generates a unique clientId
+//import { v4 as uuidv4 } from 'uuid';
+//const clientId = uuidv4(); // Generates a unique clientId
 
-export async function publishNotification(clientId, serviceId, notificationData) {
-    const client = mqtt.connect(mqttBrokerUrl, { clientId, clean: false });
-  
-    client.on('connect', async () => {
-      console.log('Publisher Connected to MQTT broker.');
-      
+export async function publishNotification(
+  clientId,
+  serviceId,
+  notificationData
+) {
+  const client = mqtt.connect(mqttBrokerUrl, { clientId, clean: false });
 
-      // Save notification to the database
-      const createdNotification = await createNotification(notificationData);
-  
-      // Publish the notification to the related service topic
-      const topic = `services/${serviceId}/notifications`;
-      const payload = JSON.stringify(createdNotification);
-      client.publish(topic, payload, {qos: 2, retain: true}, (err) => {
-        if (err) {
-          console.error(`Error publishing to topic: ${topic}`, err);
-        } else {
-          console.log(`Published notification to topic: ${topic}`);
-        }
-      });
-  
-      client.end();
-    });
-  
-    client.on('error', (err) => {
-      console.error('MQTT client error:', err);
-    });
-  
-    client.on('close', () => {
-      console.log('MQTT client disconnected.');
-    });
-  }
+  client.on("connect", async () => {
+    console.log("Publisher Connected to MQTT broker.");
 
+    // Save notification to the database
+    const createdNotification = await createNotification(notificationData);
+
+    // Publish the notification to the related service topic
+    const topic = `services/${serviceId}/notifications`;
+    const payload = JSON.stringify(createdNotification);
+    client.publish(topic, payload, { qos: 2, retain: false }, (err) => {
+      if (err) {
+        console.error(`Error publishing to topic: ${topic}`, err);
+      } else {
+        console.log(`Published notification to topic: ${topic}`);
+      }
+    });
+
+    client.end();
+  });
+
+  client.on("error", (err) => {
+    console.error("MQTT client error:", err);
+  });
+
+  client.on("close", () => {
+    console.log("MQTT client disconnected.");
+  });
+}
