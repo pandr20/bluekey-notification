1823eeb741d8dca3e7e8dc8a7923c99e2f1b788d
Rasmus <68448027+ras-e@users.noreply.github.com>
2023-05-30 02:25:32 +0200

    Added an emit spy

--- mqtt/mqttSubscriber.test.js
+++ mqtt/mqttSubscriber.test.js
@@ -13,6 +13,7 @@ jest.mock('mqtt', () => {
     on: jest.fn(),
     emit: jest.fn()
   };
+  
   return { connect: jest.fn(() => clientMock) };
 });
 
@@ -37,6 +38,11 @@ describe('subscribeUserToServices', () => {
     const clientId = 'testClient';
     const userId = 'testUser';
 
+    const clientMock = mqtt.connect(); // Create the client mock
+
+    const emitSpy = jest.spyOn(clientMock, 'emit');
+
+
     //subscribeUserToServices function
     await subscribeUserToServices(clientId, userId, client => {
       // MQTT events (made here to avoid callback error)
@@ -48,12 +54,16 @@ describe('subscribeUserToServices', () => {
         }
       });
 
+
+      
       // Testing processing of message
       client.emit('message', 'services/646a078658a06d0086007635/notifications', JSON.stringify({ id: 123 }));
 
       expect(mqtt.connect).toHaveBeenCalledWith('mqtt://mqtt:1883', { clientId, clean: false });
       expect(client.subscribe).toHaveBeenCalledWith('services/646a078658a06d0086007635/notifications', { qos: 2 });
       expect(addUserToNotification).toHaveBeenCalledWith(userId, 123);
+
+      expect(emitSpy).toHaveBeenCalledWith('message', 'services/646a078658a06d0086007635/notifications', JSON.stringify({ id: 123 }));
     });
   }, 10000); // 10 seconds
 });
